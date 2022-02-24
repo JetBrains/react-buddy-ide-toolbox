5164c4172469c4817a79ab20d5457fda8ba59a90
Roman Karandashov <r.karandashov@haulmont.com>
2022-02-24 12:27:24 +0400

    fix: checking env variable to enable devmode #15
    affects: @react-buddy/ide-toolbox

--- packages/ide-toolbox/src/config.ts
+++ packages/ide-toolbox/src/config.ts
@@ -1,2 +1,7 @@
-export const DEV_MODE = (window as any).REACT_BUDDY_IDE_DEVMODE === true 
-  || process?.env?.REACT_APP_IDE_DEVMODE === "true";
+const envDevmode = typeof process !== 'undefined'
+  ? process.env?.REACT_APP_IDE_DEVMODE === "true"
+  : false
+
+const windowDevmode = (window as any).REACT_BUDDY_IDE_DEVMODE === true;
+
+export const DEV_MODE = windowDevmode || envDevmode;
