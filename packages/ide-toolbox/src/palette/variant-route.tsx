c8cfb3bcb988004362673ec2b42cf56e3a7addb8
Lev Palkin <palkinli@haulmont.com>
2022-10-04 18:01:33 +0400

    feat: add fullScreen mode to Variant and remove viewport width and height #36

--- packages/ide-toolbox/src/palette/variant-route.tsx
+++ packages/ide-toolbox/src/palette/variant-route.tsx
@@ -1,5 +1,6 @@
 import React, {useMemo} from 'react';
 import {useRoute, PALETTE_PATH} from '../routing/routing';
+import styles from './variant-route.module.scss';
 
 const DEFAULT_VARIANT_NAME = 'DEFAULT_VARIANT';
 
@@ -7,6 +8,7 @@ export type VariantRouteProps = {
   categoryName?: string;
   componentName?: string;
   variantName?: string;
+  previewLayout?: 'center' | 'stretch';
   requiredParams?: Array<string>;
 };
 
@@ -14,6 +16,7 @@ export const VariantRoute: React.FC<VariantRouteProps> = ({
   categoryName,
   componentName,
   variantName = DEFAULT_VARIANT_NAME,
+  previewLayout = 'center',
   children,
 }) => {
   const paletteItemPath = useMemo(() => {
@@ -21,8 +24,15 @@ export const VariantRoute: React.FC<VariantRouteProps> = ({
   }, [categoryName, componentName, variantName]);
 
   const isPathMatch = useRoute(paletteItemPath, true);
-  return isPathMatch ? <>{children}</> : null;
-};
+
+  if (!isPathMatch) {
+    return null;
+  }
+
+  return previewLayout === 'center'
+    ? <div className={styles.variantRouteCenter}>{children}</div>
+    : <>{children}</>
+}
 
 function getPaletteItemPath(names: Array<string | null | undefined>): string {
   return PALETTE_PATH + '/' + names.filter((name) => name != null).join('/');
